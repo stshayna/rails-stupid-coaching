89dfe99de097bad21e885d41ee64785d6cdd7921
stshayna@gmail.com <stshayna@gmail.com>
2022-04-05 20:50:09 -0400

    pages complete w/o CSS 5 Apr 2022

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,9 +2,10 @@ class QuestionsController < ApplicationController
   def ask; end
 
   def answer
-    @answer = if params[:question] == 'I am going to work'
+    @question = params[:question]
+    @answer = if @question == 'I am going to work'
                 'Great!'
-              elsif params[:question].end_with?('?')
+              elsif @question.end_with?('?')
                 'Silly question, get dressed and go to work!'
               else
                 'I don\'t care, get dressed and go to work!'
